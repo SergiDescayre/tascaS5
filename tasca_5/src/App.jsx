d1f35fbd3f88dca06c3343cba7213788bca1d377
SergiDescayre <lepetitdesca@gmail.com>
2024-02-26 18:29:08 +0100

    exercici 3 i 4

--- tasca_5/src/App.jsx
+++ tasca_5/src/App.jsx
@@ -12,34 +12,39 @@ const App = () => {
         setStep( prev => prev + 1 );
     }
   };
+  const prevStep = () => {
+    if(step  != 0){
+        setStep( prev => prev - 1 );
+    }
+  };
 
   let tutorialData = [
     {
       title: "Dedica moltes hores",
       description:
         "Un mínim de 30 hores a la setmana. Si no en tens prou, hauràs de dedicar-li més hores. Al rpincipi sembla impossibe, però notaras una millora rapidament.",
-      bgColor: "red",
+      bgColor: "#4da2a9",
       image: meditation,
     },
     {
       title: "Programa projectes propis",
       description:
-        "dMés vañ 10 hores treballamt en projectes propis que 10 hores mirant tutorials. La motvació i la implicació en el projecte ajudarà a accelerar el teu aprenentate.",
-      bgColor: "red",
+        "Més val 10 hores treballamt en projectes propis que 10 hores mirant tutorials. La motivació i la implicació en el projecte ajudarà a accelerar el teu aprenentate.",
+      bgColor: "#d3d4d9",
       image: programming,
     },
     {
       title: "Procura descansar",
       description:
         "Descansar bé i desconectar son vitals. D'aquest manera reduiràs l'estrés i l'ansietat. Millorars la teva concentració i consolidaras el teu aprenentatge.",
-      bgColor: "red",
+      bgColor: "#ffd167",
       image: time,
     },
   ];
 
   return (
     <>
-      <Card step={step} tutorialData={tutorialData} nextStep={nextStep} />
+      <Card step={step} tutorialData={tutorialData} nextStep={nextStep} prevStep={prevStep}/>
     </>
   );
 };
--- tasca_5/src/components/Card.jsx
+++ tasca_5/src/components/Card.jsx
@@ -1,12 +1,53 @@
-const Card = ({ step, tutorialData, nextStep }) => {
+import { ArrowRight, ArrowLeft } from "react-bootstrap-icons";
+
+
+
+const Card = ({ step, tutorialData, nextStep,prevStep }) => {
+  console.log("paso " + step)
+  console.log(tutorialData.length-1)
   return (
     <>
-      <img src={tutorialData[step].image} alt="" />
-      <h3>{tutorialData[step].title}</h3>
-      <span>{tutorialData[step].description}</span>
-      <div>
-       
-        <button onClick={nextStep}>Next step</button>
+      <div className="row">
+        <div className="col-sm-8 col-md-5 col-lg-5  mx-auto">
+          <div className="card shadow rounded-3 ">
+            <div
+              className="container_img rounded-top pb-3"
+              style={{ backgroundColor: tutorialData[step].bgColor }}
+            >
+              <img
+                src={tutorialData[step].image}
+                alt={tutorialData[step].title}
+                height={200}
+                width={200}
+              />
+            </div>
+            <div className="card-body" >
+              <h6 className="card-title">{tutorialData[step].title}</h6>
+              <p className="card-text">{tutorialData[step].description}</p>
+              <div className="d-flex justify-content-between" >
+                <div className="d-flex gap-2 align-items-end" >
+                  <span>.</span>
+                  <span>.</span>
+                  <span>.</span>
+                </div>
+                <div className="d-flex gap-1">
+                  <button
+                    className={step===0 ? "hide" : "btn_arrow btn btn-outline-dark rounded-circle" }
+                    onClick={prevStep}
+                  >
+                    <ArrowLeft />
+                  </button>
+                  <button
+                    className={(tutorialData.length-1 === step) ? "hide" : "btn_arrow btn btn-dark rounded-circle"}
+                    onClick={nextStep}
+                  >
+                    <ArrowRight />
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );

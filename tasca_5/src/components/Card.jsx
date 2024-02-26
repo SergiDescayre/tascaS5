6fc436ba39fe2d61513779c2639003c122d4456a
SergiDescayre <lepetitdesca@gmail.com>
2024-02-26 18:43:26 +0100

    arreglo responsive

--- tasca_5/src/components/Card.jsx
+++ tasca_5/src/components/Card.jsx
@@ -1,14 +1,12 @@
 import { ArrowRight, ArrowLeft } from "react-bootstrap-icons";
 
-
-
-const Card = ({ step, tutorialData, nextStep,prevStep }) => {
-  console.log("paso " + step)
-  console.log(tutorialData.length-1)
+const Card = ({ step, tutorialData, nextStep, prevStep }) => {
+  console.log("paso " + step);
+  console.log(tutorialData.length - 1);
   return (
     <>
       <div className="row">
-        <div className="col-sm-8 col-md-5 col-lg-5  mx-auto">
+        <div className="col-sm-8 col-md-5 col-lg-4 col-xl-3  mx-auto">
           <div className="card shadow rounded-3 ">
             <div
               className="container_img rounded-top pb-3"
@@ -21,24 +19,32 @@ const Card = ({ step, tutorialData, nextStep,prevStep }) => {
                 width={200}
               />
             </div>
-            <div className="card-body" >
+            <div className="card-body">
               <h6 className="card-title">{tutorialData[step].title}</h6>
               <p className="card-text">{tutorialData[step].description}</p>
-              <div className="d-flex justify-content-between" >
-                <div className="d-flex gap-2 align-items-end" >
+              <div className="d-flex justify-content-between">
+                <div className="d-flex gap-2 align-items-end">
                   <span>.</span>
                   <span>.</span>
                   <span>.</span>
                 </div>
                 <div className="d-flex gap-1">
                   <button
-                    className={step===0 ? "hide" : "btn_arrow btn btn-outline-dark rounded-circle" }
+                    className={
+                      step === 0
+                        ? "hide"
+                        : "btn_arrow btn btn-outline-dark rounded-circle"
+                    }
                     onClick={prevStep}
                   >
                     <ArrowLeft />
                   </button>
                   <button
-                    className={(tutorialData.length-1 === step) ? "hide" : "btn_arrow btn btn-dark rounded-circle"}
+                    className={
+                      tutorialData.length - 1 === step
+                        ? "hide"
+                        : "btn_arrow btn btn-dark rounded-circle"
+                    }
                     onClick={nextStep}
                   >
                     <ArrowRight />
